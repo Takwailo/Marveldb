f5a6ec3797a4838704f71f58f173ca61fe834882
takwai <takwai0318@gmail.com>
2022-04-27 19:02:23 -0400

    added Movie Models

--- main_app/models.py
+++ main_app/models.py
@@ -13,3 +13,12 @@ class Heros(models.Model):
 
     def get_absolute_url(self):
         return reverse('hero_detail', kwargs={'hero_id': self.id})
+
+class Movie(models.Model):
+    movie_name = models.CharField(max_length=50)
+    release_date = models.DateFiled()
+
+    hero = models.ForeignKey(Heros, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.movie_name} released on {self.release_date}"
